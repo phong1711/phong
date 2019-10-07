3bb39793d3ce645d2354220f5458525d83eb9004
Phong <phong@Phongs-Air.net.fpt>
2019-10-07 18:34:09 +0800

    day01

---
+++ File.swift
@@ -0,0 +1,50 @@
+//
+//  File.swift
+//  baitap1
+//
+//  Created by Phong on 10/7/19.
+//  Copyright © 2019 Phong. All rights reserved.
+////
+// bài tâp 1
+import Foundation
+//func tinhdientich(R: Double) -> Double{
+//    var S: Double
+//    S = (4 * π * R)
+//    print("diện tích hình cầu ")
+//    return pow(S,2)
+//}
+//
+//func tinhthetich(R: Double) -> Double{
+//    var V: Double
+//    V = ((4/3) * π * R)
+//    print("thể tích hình cầu ")
+//    return pow(V,3)
+//}
+
+
+
+//// bài tập 2
+//func binhphuonga(a: Double) -> Double{
+//return pow(a,2)
+//}
+//func binhphuongb(b: Double) -> Double{
+//return pow(b,2)
+//    }
+//func tongbinhphuong(c:Double, d: Double) -> Double{
+//    var S: Double
+//    S = (c + d)
+//    print("tổng bình phương a và b")
+//    return S
+//}
+//
+
+
+
+
+//// bài tập 6
+//func phuongtrinh(a: Double,b: Double) -> Double{
+//    var x: Double
+//    x = -(a/b)
+//    print("ax + b =")
+//    return x
+//}
---
+++ main.swift
@@ -0,0 +1,85 @@
+//
+//  main.swift
+//  baitap1
+//
+//  Created by Phong on 10/7/19.
+//  Copyright © 2019 Phong. All rights reserved.
+//
+
+
+//bài tập 1
+import Foundation
+//let π = 3.14
+//
+//var S = tinhdientich
+//var dientich = tinhdientich(R: 21)
+//print(dientich)
+//var V = tinhthetich
+//var thetich = tinhthetich(R: 21)
+//print(thetich)
+
+
+////// bài tập 2
+//var c = binhphuonga(a: 4)
+//print(c)
+//var d = binhphuongb(b: 5)
+//print(d)
+//var S = tongbinhphuong
+//var tongbinhphuongab = tongbinhphuong(c: 16, d: 25)
+//print(tongbinhphuongab)
+
+
+
+
+////bài tập 3
+//let n: Int = (1*5)
+//if  (n > 0) {
+//    print("n là số nguyên dương")
+//}
+//else {
+//print("n không phải là số nguyên dương")
+//}
+//
+//if n / 2 == 0 {
+//print("n là số chẵn")
+//}
+//else {
+//print("n là số lẻ")
+//}
+////
+//if n / 5 == 1 {
+//    print("n chia hết cho 5")
+//}
+//else {
+//print("n không chia hết cho 5")
+//}
+//if (n > 1) {
+//print("n là số nguyên tố")
+//}
+//else if (n < 1){
+//print("n không phải số nguyên tố")
+//}
+
+//bài tập 4
+//let a: Int = 4
+//let b: Int = 4
+//if (a / b) == 1 {
+//print("a chia hết cho b")}
+//else {
+//print("a không chia hết cho b")}
+//
+
+
+
+//bài tập 5
+//let a = 7
+//let b = -5
+//if (a>b){
+//print("max là \(a)")}
+//else if (a<b) {
+//print("max là \(b)")
+//}
+
+//bài tập 6
+//var x = phuongtrinh(a: 4, b: 5)
+//print(x)
